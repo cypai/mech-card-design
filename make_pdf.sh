e3c63b280658d9367f211bfe6b1191ec8b8e3f11
Cheng-Yu Pai <mail4charliepai@gmail.com>
2025-07-17 02:43:09 -0700

    make pdfs of equipment and mechs

--- make_pdf.sh
+++ make_pdf.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-montage outputs/equipment/*.png -tile 3x3 -geometry 750x1050 -background white -density 300 outputs/montages/output.png
+montage outputs/equipment/*.png -tile 3x3 -geometry 750x1050 -background white -density 300 outputs/equipment_montages/output.png
 
-magick convert outputs/montages/*.png -gravity Center outputs/montages/output.pdf
+magick convert outputs/equipment_montages/*.png -gravity Center equipment.pdf
+
+montage outputs/mechs/*.png -tile 3x3 -geometry 750x1050 -background white -density 300 outputs/mechs_montages/output.png
+
+magick convert outputs/mechs_montages/*.png -gravity Center mechs.pdf
