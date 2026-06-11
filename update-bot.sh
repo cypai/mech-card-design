16248dcdb444df0694f301f17bcbb8cc329358da
Cheng-Yu Pai <mail4charliepai@gmail.com>
2026-06-11 03:19:58 -0700

    adjust update-bot script slightly

--- update-bot.sh
+++ update-bot.sh
@@ -3,13 +3,13 @@
 git pull
 source ./venv/bin/activate
 
-read -p "Render cards? y: all, n: skip, c: custom" ync
+read -p "Render cards? (y: all, n: skip, c: custom)" ync
 case $ync in
   [yY] )
     ./card_rendering.py all
     ;;
   [cC] )
-    read "What would you like to render?" op
+    read "What would you like to render? " op
     ./card_rendering.py $op
     ;;
   [nN] ) echo "Skipping render.";;
