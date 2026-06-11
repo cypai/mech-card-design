b3ed17a2d790f6e0a90c3bc8262a70535b9e08fe
Cheng-Yu Pai <mail4charliepai@gmail.com>
2026-06-11 01:21:18 -0700

    Add update-bot script

---
+++ update-bot.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+git pull
+source ./venv/bin/activate
+
+read -p "Render cards? y: all, n: skip, c: custom" ync
+case $ync in
+  [yY] )
+    ./card_rendering.py all
+    ;;
+  [cC] )
+    read "What would you like to render?" op
+    ./card_rendering.py $op
+    ;;
+  [nN] ) echo "Skipping render.";;
+  * ) echo "Invalid response";;
+esac
+
+sudo systemctl restart discord-bot.service
